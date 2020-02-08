82ed4c8b31b41140af9a6102577bc8a15452463a
q101545@163.com <mac@macs-MacBook-Pro.local>
2020-02-09 00:23:00 +0800

    first submit

---
+++ config/config.default.js
@@ -0,0 +1,33 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1581173903069_1800';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+    token: 'XXXXXX',
+    appid:'XXXXXXX',
+    encodingAESKey: 'XXXXXX'
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
