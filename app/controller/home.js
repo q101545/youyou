bf0cc5923e9b1ec33ceb22604022dc476ad90d85
q101545@163.com <mac@macs-MacBook-Pro.local>
2020-02-09 19:31:01 +0800

    log

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,15 +6,17 @@ var sha1 = require('sha1')
 class HomeController extends Controller {
      async index() {
          var obj = this.ctx.query
+         this.logger.info(obj)
          var token = this.config.token
-         timestamp = obj.timestamp,
-         nonce = obj.nonce,
-         echostr = obj.echostr,
-         signature = obj.signature,
-         str = [token, timestamp, nonce].sort().join(''),
-         sha = sha1(str);
+         timestamp = obj.timestamp
+         nonce = obj.nonce
+         echostr = obj.echostr
+         signature = obj.signature
+         str = [token, timestamp, nonce].sort().join('')
+         sha = sha1(str)
          if(sha === signature){
              this.ctx.body = echostr + ''
+             this.logger.info("调用成功")
          }
      }
  }
